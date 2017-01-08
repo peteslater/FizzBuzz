a567d60dd27a5c6c642d39f9e3dd4a2f908f6517
Pete Slater <peteslater@gmail.com>
2017-01-08 08:10:35 +0000

    Added check for integer arguments in App

--- src/main/scala/FizzBuzzApp.scala
+++ src/main/scala/FizzBuzzApp.scala
@@ -1,3 +1,10 @@
+import scala.util.Try
+
 object FizzBuzzApp extends App {
-  println(FizzBuzz.adapt(args.map(_.toInt).toList))
+	if (args.forall(x => Try(x.toInt).isSuccess))
+  	println(FizzBuzz.adapt(args.map(_.toInt).toList))
+	else {
+		println("All arguments should be integers")
+		println(s"You provided ${args.mkString(" ")}")
+	}
 }
